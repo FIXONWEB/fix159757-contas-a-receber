fbf0715ec9f21b2f3ddd1580871176e69da2e5ee
fixonweb <contato@fixonweb.com.br>
2020-08-16 13:48:16 -0300

    0.1.5.2

--- fix159757-contas-a-receber.php
+++ fix159757-contas-a-receber.php
@@ -7,7 +7,7 @@
  * Author URI:      https://fixonweb.com.br
  * Text Domain:     fix159757-contas-a-receber
  * Domain Path:     /languages
- * Version:         0.1.4
+ * Version:         0.1.5
  *
  * @package         Fix159757_Contas_A_Receber
  */
